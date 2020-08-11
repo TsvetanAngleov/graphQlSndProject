6831a1a1e9137dd21f036a3dc3ab073a29f97bc9
Tsvetan Angelov <tsvetan@izpitai.me>
2020-08-11 08:49:30 +0300

    Finished

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -41,7 +41,9 @@ const mutation = gql`
     addLyricToSong(content: $content, songId: $songId) {
       id
       lyrics {
+        id
         content
+        likes
       }
     }
   }
--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
 class LyricList extends Component {
-  likeClickHandler(id) {
-    console.log(id);
+  likeClickHandler(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typename: "Mutation",
+        likeLyric: {
+          id: id,
+          __typename: "LyricType",
+          likes: likes + 1,
+        },
+      },
+    });
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
-          <i
-            className="material-icons"
-            onClick={() => this.likeClickHandler(id)}
-          >
-            thumb_up
-          </i>
+          <div className="vote-box">
+            <i
+              className="material-icons"
+              onClick={() => this.likeClickHandler(id, likes)}
+            >
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       );
     });
@@ -25,4 +40,13 @@ class LyricList extends Component {
   }
 }
 
-export default LyricList;
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
